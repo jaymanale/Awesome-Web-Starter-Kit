ea44ad594c53b52fc57d571448badb527ab76d12
jaymanale <jaymanale29@gmail.com>
2017-04-19 21:40:40 +0530

    Gulp first task created //printing log

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+// gulp package
+
+var gulp=require('gulp'),
+    gutil=require('gulp-util');
+
+// creating default task
+
+gulp.task('default',function(){
+
+    return gutil.log("Gulp work. Awesome(o_o)");
+});
